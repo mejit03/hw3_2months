b4b705b9d46709af7cc338d59852f749aa707c11
mejit03 <dtghjgfhfdh@gmail.com>
2022-11-30 14:11:29 +0300

    dz3

---
+++ app.js
@@ -0,0 +1,59 @@
+// const a = prompt('Введите первое число');
+// const b = prompt('Введите второе число');
+
+// if(a > b) {
+//     console.log(b);
+// }else if(a < b) {
+//     console.log(a);
+// }else{
+//     console.log("ошибка");
+// }
+
+
+
+// const param = prompt();
+// console.log(param.length);
+
+
+
+
+// const num1 = parseInt(prompt('Введите первое число'));
+// const operation = prompt("Выберите действие\n+\n-\n/\n*");
+// const num2 = parseInt(prompt('Введите второе число'));
+
+// function sum(num1, num2) {
+//     alert(num1 + num2)
+// }
+// function minus(num1, num2) {
+//     alert(num1 - num2)
+// }
+// function multiply(num1, num2) {
+//     alert(num1 * num2)
+// }
+// function divide(num1, num2) {
+//     alert(num1 / num2)
+// }
+
+// switch(operation) {
+//     case "+":
+//     sum(num1, num2) 
+//     break;
+    
+//     case "-":
+//     minus(num1, num2) 
+//     break;
+
+//     case "*":
+//     multiply(num1, num2) 
+//     break;
+
+//     case "/":
+//     divide(num1, num2) 
+//     break;
+
+//     default :
+//     alert("Вводить только числа")
+// } 
+
+
+
